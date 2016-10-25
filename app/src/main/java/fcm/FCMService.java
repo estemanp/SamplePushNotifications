9d35302f073f5e1f15ae77f8864351ddd8dc52c8
estemanp <estemanp@gmail.com>
2016-10-24 20:59:04 -0500

    GRAL: se agrega el pry de ejemplo de notificaciones push

---
+++ app/src/main/java/fcm/FCMService.java
@@ -0,0 +1,62 @@
+package fcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.programacion.movil.estemanp.samplepushnotifications.MainActivity;
+import com.programacion.movil.estemanp.samplepushnotifications.R;
+
+import java.util.Map;
+
+public class FCMService extends FirebaseMessagingService {
+    private static final String TAG = FCMService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "¡Mensaje recibido!");
+        displayNotification(remoteMessage.getNotification(), remoteMessage.getData());
+        sendNewPromoBroadcast(remoteMessage);
+    }
+
+    private void sendNewPromoBroadcast(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(MainActivity.ACTION_NOTIFY_NEW_PROMO);
+        intent.putExtra("title", remoteMessage.getNotification().getTitle());
+        intent.putExtra("description", remoteMessage.getNotification().getBody());
+        intent.putExtra("discount", remoteMessage.getData().get("discount"));
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .sendBroadcast(intent);
+    }
+
+    //este metodo genera la push notification
+    private void displayNotification(RemoteMessage.Notification notification, Map<String, String> data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_bell)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+}
